a350a628b7724f162c5d6510fb89d1869845accf
Stefan Safranek <sjsafranek@gmail.com>
2024-08-15 18:14:58 +0100

    load config file

--- forms/app.py
+++ forms/app.py
@@ -10,31 +10,33 @@ DEFAULT_SECRET_KEY = "skeleton"
 DEFAULT_SESSION_TYPE = "filesystem"
 
 
-config_file = 'config.json'
-config = {}
-with open(config_file) as fh:
-	config = json.load(fh)
-print(config)
+def load_config(filename):
+	with open(filename) as fh:
+		return json.load(fh)
 
 
-app = Flask(__name__)
-app.secret_key = config.get("secret_key", DEFAULT_SECRET_KEY)
-app.config['SESSION_TYPE'] = config.get("session_type", DEFAULT_SESSION_TYPE)
+def create_app(config_file = 'config.json'):
 
+	config = load_config(config_file)
 
+	app = Flask(__name__)
+	app.secret_key = config.get("secret_key", DEFAULT_SECRET_KEY)
+	app.config['SESSION_TYPE'] = config.get("session_type", DEFAULT_SESSION_TYPE)
 
-forms = config.get("forms", {})
-for endpoint in forms:
-	form = forms[endpoint]
-	options = form.get("options", {})
+	forms = config.get("forms", {})
+	for endpoint in forms:
+		form = forms[endpoint]
+		options = form.get("options", {})
 
-	blueprint = None
-	if "blogpost" == form["type"]:
-		blueprint = blogpost.new(options)
-	elif "fileupload" == form["type"]:
-		blueprint = fileupload.new(options)
-	elif "dynamic" == form["type"]:
-		blueprint = dynamic.new(options)
+		blueprint = None
+		if "blogpost" == form["type"]:
+			blueprint = blogpost.new(options)
+		elif "fileupload" == form["type"]:
+			blueprint = fileupload.new(options)
+		elif "dynamic" == form["type"]:
+			blueprint = dynamic.new(options)
 
-	app.register_blueprint(blueprint, url_prefix=endpoint)
+		app.register_blueprint(blueprint, url_prefix=endpoint)
+
+		return app
 
